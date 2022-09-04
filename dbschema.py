56c2401ab7e4e3ba89e3a8e8e96699d3cb06afe9
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-09-04 10:08:47 -0400

    commit db schema and constants

---
+++ dbschema.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+from sqlalchemy.orm import registry
+
+
+mapper_registry = registry()
+
+
+@mapper_registry.mapped
+@dataclass
+class CalendarSubscription:
+	__table__ = Table(
+        "calendar_subscription",
+        mapper_registry.metadata,
+        Column("cg_group_id", Integer, primary_key=True),
+        Column("discord_server_id", Integer),
+    )
+	cg_group_id:int
+	discord_server_id: int
