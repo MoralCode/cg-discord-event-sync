258d65cbdbce01f491d1e5669facdb0ba9a8435f
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-09-04 11:13:24 -0400

    add basic campusgroups file that can scrape a list of groups

---
+++ campusgroups.py
@@ -0,0 +1,23 @@
+# file for string campusgroups querying finctions
+
+
+from bs4 import BeautifulSoup
+import requests
+import re
+
+CG_ALL_GROUPS_LIST_URL="https://campusgroups.rit.edu/ics_helper"
+
+def get_all_campus_groups():
+	result = requests.get(CG_ALL_GROUPS_LIST_URL)
+	grouplisthtml = BeautifulSoup(result.content, 'html.parser') 
+
+	groupsdiv = grouplisthtml.find('div', attrs={'aria-label': 'Group Calendars.'})
+	groupstable = groupsdiv.find('table')
+	tablerows = groupstable.find_all("tr")
+	for row in tablerows:
+		data = row.find_all("td")
+		name = data[0].contents[0]
+		identifier = data[-1].find("a")["href"]
+		x = re.search("ical_club_[0-9]*\.ics", identifier) 
+		identifier = int("".join(filter(str.isdigit, x[0])))
+
