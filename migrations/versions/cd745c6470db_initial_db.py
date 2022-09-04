e4756d2bc73b955faeb60fc8ad88ef9d71ed5dd2
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-09-04 10:15:11 -0400

    create initial alembic db migration

---
+++ migrations/versions/cd745c6470db_initial_db.py
@@ -0,0 +1,24 @@
+"""initial DB
+
+Revision ID: cd745c6470db
+Revises: 
+Create Date: 2022-09-04 10:14:52.080918
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cd745c6470db'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
